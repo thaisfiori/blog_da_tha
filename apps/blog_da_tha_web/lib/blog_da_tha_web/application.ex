e1d489bdb0c49db9bae940e2a63f6f8be90e498e
Thais Fiori <thais.fiori@stone.com.br>
2021-04-14 11:51:03 -0300

    Fixing projects details

--- apps/blog_da_tha_web/lib/blog_da_tha_web/application.ex
+++ apps/blog_da_tha_web/lib/blog_da_tha_web/application.ex
@@ -1,28 +1,19 @@
 defmodule BlogDaThaWeb.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
+
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Start the Telemetry supervisor
       BlogDaThaWeb.Telemetry,
-      # Start the Endpoint (http/https)
       BlogDaThaWeb.Endpoint
-      # Start a worker by calling: BlogDaThaWeb.Worker.start_link(arg)
-      # {BlogDaThaWeb.Worker, arg}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: BlogDaThaWeb.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   def config_change(changed, _new, removed) do
     BlogDaThaWeb.Endpoint.config_change(changed, removed)
     :ok
